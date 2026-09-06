b4fb0058402026e55752e1b50b0be841e20e3174
ROHIT RASTOGI <rohit2000rastogi@gmail.com>
2026-09-06 20:57:22 +0530

    Delete Backend directory

--- Backend/routes/matchingRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { matchWorker } from "../controllers/matchingController.js";
-
-const router = express.Router();
-
-router.post("/match", matchWorker);
-
-export default router;
