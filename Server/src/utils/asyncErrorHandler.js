9be684dc06b2e46fe063d3900832e99cb8581478
ROHIT RASTOGI <rohit2000rastogi@gmail.com>
2026-09-19 19:22:55 +0530

    Delete Server directory

--- Server/src/utils/asyncErrorHandler.js
+++
@@ -1,9 +0,0 @@
-//* if any error found in Middleware code in express, so this code helps to find the exact error.
-
-const asyncErrorHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
-};
-
-export { asyncErrorHandler };
